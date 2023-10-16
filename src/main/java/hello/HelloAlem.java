311b0ec75d8119d6a21fa6b2343f5d404605b1d0
Sanzhar Suleimenov <sanzhar.suleimenov@icloud.com>
2023-10-16 22:25:32 +0600

    left only postgres related classes and code

--- src/main/java/hello/HelloAlem.java
+++ src/main/java/hello/HelloAlem.java
@@ -1,17 +1,11 @@
 package hello;
 
-import org.joda.time.LocalTime;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class HelloAlem {
   public static void main(String[] args) {
-    LocalTime currentTime = new LocalTime();
-    System.out.println("The current local time is: " + currentTime);
-    Greeter greeter = new Greeter();
-    System.out.println(greeter.sayHello());
-
     List<Store> result = new ArrayList<>();
 
     String SQL_SELECT = "select * from store";
